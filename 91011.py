36e476fa9bc8707cd5dc10010cc39d747bd83005
rapidofc1 <33822216+rapidofc1@users.noreply.github.com>
2017-12-22 17:39:12 -0700

    Update 91011.py

--- 91011.py
+++ 91011.py
@@ -1,4 +1,4 @@
-import openweathermapy.core as weather
+#import openweathermapy.core as weather
 import unicodedata
 import youtube_dl
 import pyowm
@@ -530,32 +530,6 @@ async def coinflip():
        await bot.say("**:hear_no_evil:  |  Heads!**")
     if choice == 2:
        await bot.say("**:monkey:  |  Tails!**")
-
-@bot.command(pass_context=True)
-async def weather(ctx, *, city: str):
-    settings = {"APPID": '5793b69ec91fb3232c200c1df4c2141b'}
-    data = weather.get_current('{}'.format(city), units='metric', **settings)
-    data2 = weather.get_current(city, units='standard', **settings)
-    keys = ['main.temp', 'main.humidity', 'coord.lon', 'coord.lat']
-    x = data.get_many(keys)
-    loc = data('name')
-    country = data('sys.country')
-    lon = data('coord.lon')
-    lat = data('coord.lat')
-    temp = data('main.temp')
-    temp2 = temp * 9/5 + 32
-    high = data('main.temp_max')
-    low = data('main.temp_min')
-    high2 = high * 9/5 + 32
-    low2 = low * 9/5 + 32
-    embed = discord.Embed(title = '{}, {}'.format(loc, country), color = 0x00FF00)
-    embed.add_field(name = 'Absolute Location', value = 'Longitude, Latitude\n{}, {}'.format(lon, lat))
-    embed.add_field(name = 'Temperature', value = '{}F, {}C'.format(temp2, temp))
-    embed.add_field(name = 'Humidity', value = '{}%'.format(data('main.humidity')))
-    embed.add_field(name = 'Wind Speed', value = '{}m/s'.format(data('wind.speed')))
-    embed.add_field(name = 'Low and High Temp', value = '{}F - {}F\n{}C - {}C'.format(low2, high2, low, high))
-    embed.set_footer(text = 'Weather Data')
-    await bot.say(embed = embed)
 
 @bot.command()
 async def potatos():
