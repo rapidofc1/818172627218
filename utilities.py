09385e53e87516102c957e19aa2cf7755e265954
rapidofc1 <33822216+rapidofc1@users.noreply.github.com>
2017-12-24 15:59:47 -0700

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,22 +1,22 @@
-import discord
-
-
-async def error_embed(msg):
-    e = discord.Embed(color=discord.Color.red())
-    e.set_author(name="🚫 Something went wrong!")
-    e.description = msg
-    return e
-
-
-async def success_embed(msg):
-    e = discord.Embed(color=discord.Color.green())
-    e.set_author(name="✔️ Success!")
-    e.description = msg
-    return e
-
-
-async def wip_embed():
-    e = discord.Embed(color=discord.Color.dark_orange())
-    e.set_author(name="🛠️ This command is under construction.")
-    e.description = "Unfortunately, this command isn't available just yet. Try again at a later date."
-    return e
+#import discord
+#
+#
+#async def error_embed(msg):
+#    e = discord.Embed(color=discord.Color.red())
+#    e.set_author(name="🚫 Something went wrong!")
+#    e.description = msg
+#    return e
+#
+#
+#async def success_embed(msg):
+#    e = discord.Embed(color=discord.Color.green())
+#    e.set_author(name="✔️ Success!")
+#    e.description = msg
+#    return e
+#
+#
+#async def wip_embed():
+#    e = discord.Embed(color=discord.Color.dark_orange())
+#    e.set_author(name="🛠️ This command is under construction.")
+#    e.description = "Unfortunately, this command isn't available just yet. Try again at a later date."
+#    return e
