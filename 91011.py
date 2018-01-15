2b0992eb61bec2bdd39f816e5647a7cf8bd2212a
rapidofc1 <33822216+rapidofc1@users.noreply.github.com>
2018-01-15 07:30:07 -0700

    Update 91011.py

--- 91011.py
+++ 91011.py
@@ -350,7 +350,7 @@ async def starboard(ctx,*, message: str):
     react2 = await bot.send_message(bot.get_channel("393819606344663049"), content = "**:star: " + ctx.message.server.name + " ID: "+ ctx.message.server.id + "**", embed = embed)
     react2 = await bot.send_message(bot.get_channel("397738520887689227"), content = "**:star: " + ctx.message.server.name + " ID: "+ ctx.message.server.id + "**", embed = embed)
     react2 = await bot.send_message(bot.get_channel("400788192833437706"), content = "**:star: " + ctx.message.server.name + " ID: "+ ctx.message.server.id + "**", embed = embed)
-#    react2 = await bot.send_message(bot.get_channel(""), content = "**:star: " + ctx.message.server.name + " ID: "+ ctx.message.server.id + "**", embed = embed)
+    react2 = await bot.send_message(bot.get_channel("402329246141251585"), content = "**:star: " + ctx.message.server.name + " ID: "+ ctx.message.server.id + "**", embed = embed)
     await bot.add_reaction(msg2send, "⭐")
 #    await bot.add_reaction(react2, "⭐")
     await bot.say("**:white_check_mark: | " + ctx.message.author.name + ", I've published your message!**")
